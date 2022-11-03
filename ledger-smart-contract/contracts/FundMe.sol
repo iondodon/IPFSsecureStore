75b1543cf00e9131646bff252aa54ec592d015f9
Ion Dodon <iondodon2@gmail.com>
2022-11-03 14:41:25 +0200

    simplified

--- ledger-smart-contract/contracts/FundMe.sol
+++ ledger-smart-contract/contracts/FundMe.sol
@@ -1,15 +1,8 @@
 // SPDX-License-Identifier: MIT
-// 1. Pragma
 pragma solidity ^0.8.7;
 
-// 3. Interfaces, Libraries, Contracts
 error FundMe__NotOwner();
 
-/**@title A sample Funding Contract
- * @author Patrick Collins
- * @notice This contract is for creating a sample funding contract
- * @dev This implements price feeds as our library
- */
 contract FundMe {
     mapping(address => uint256) private s_addressToAmountFunded;
 
