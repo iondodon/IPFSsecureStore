022a3c95ceee1782dfe7e04da3c2a874eba36cc4
Ion Dodon <iondodon2@gmail.com>
2022-11-03 14:39:18 +0200

    simplified

--- ledger-smart-contract/deploy/01-deploy-fund-me.js
+++ ledger-smart-contract/deploy/01-deploy-fund-me.js
@@ -8,18 +8,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
-    let ethUsdPriceFeedAddress
-    if (chainId == 31337 || chainId == 1337) {
-        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
-        ethUsdPriceFeedAddress = ethUsdAggregator.address
-    } else {
-        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
-    }
-    log("----------------------------------------------------")
     log("Deploying FundMe and waiting for confirmations...")
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress],
+        args: [],
         log: true,
         // we need to wait if on a live network so we can verify properly
         waitConfirmations: network.config.blockConfirmations || 1,
@@ -30,7 +22,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
     ) {
-        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+        await verify(fundMe.address, [])
     }
 }
 
